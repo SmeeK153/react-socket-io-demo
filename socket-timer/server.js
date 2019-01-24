fa4567ce2fc90c0757b77dce76de20ee4bc6d85f
Eric Freeman <eric.freeman@mindbodyonline.com>
2019-01-24 08:05:00 -0800

    Configure server for emitting messages

--- socket-timer/server.js
+++ socket-timer/server.js
@@ -2,8 +2,13 @@ const io = require('socket.io')();
 const PORT = 8000;
 
 io.on('connection', (client) => {
-    // emit events here
+    client.on('subscribeToTimer', (interval) => {
+        console.log(`Client is subscribing to timer at the interval ${interval}.`);
+        setInterval(() => {
+            client.emit('timer', new Date());
+        }, interval);
+    });
 });
 
 io.listen(PORT);
-console.log(`Listening on port ${PORT}`);
+console.log(`Listening on port ${PORT}.`);
